dab4d0b25a1654227824555828774c8ef60f1d32
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-14 23:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  let animal = "cat"
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+
 
 let two = 2
 
